9f373d22c835567770b2fec44dae88cf77ce7e95
brett weisberg <weisberg.brett@gmail.com>
2017-12-10 15:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+var leastFavoriteCustomer = "doug";
+
+function upperCaseCustomerName () {
+customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer () {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  LeastFavoriteCustomer = "tom";
+}
+
+function favoriteCustomer() {
+  const favoriteCustomer = "Hisa";
+  favoriteCustomer = "Brett";
+}
